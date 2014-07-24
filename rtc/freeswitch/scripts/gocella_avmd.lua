1030b7afcc354168c434204cd86e1670fc5e8e41
Jean Chassoul <chassoul@gmail.com>
2014-07-24 13:24:35 -0600

    change on gocella avmd script

--- rtc/freeswitch/scripts/gocella_avmd.lua
+++ rtc/freeswitch/scripts/gocella_avmd.lua
@@ -2,14 +2,31 @@ local human_detected = false;
 local voicemail_detected = false;
  
 function onInput(session, type, obj)
+ 
+    if type == "dtmf" and obj['digit'] == '0' and human_detected == false then
+        human_detected = true;
+        session:execute("avmd","stop");
+        session:execute("transfer","2005");
+        return "break";
+    end
+
     if type == "dtmf" and obj['digit'] == '1' and human_detected == false then
         human_detected = true;
-        session:streamFile("/usr/local/freeswitch/sounds/gocella/voicemail.wav");
+        session:execute("avmd","stop");
+        session:execute("transfer","2005");
+        return "break";
+    end
+
+    if type == "dtmf" and obj['digit'] == '9' and human_detected == false then
+        human_detected = true;
+        session:execute("avmd","stop");
         return "break";
     end
  
     if type == "event" and voicemail_detected == false then
         voicemail_detected = true;
+        session:execute("avmd","stop");
+        session:streamFile("/usr/local/freeswitch/sounds/gocella/voicemail.wav");
         return "break";
     end
 end
@@ -20,7 +37,6 @@ session:answer();
 session:setInputCallback("onInput");
 session:execute("avmd","start");
 
-
 -- sleep a second
 session:sleep(1000);
 
@@ -28,7 +44,6 @@ session:sleep(1000);
 session:streamFile("/usr/local/freeswitch/sounds/gocella/studentloan.wav");
 
 session:execute("avmd","stop");
-
 
 -- hangup
 session:hangup();
